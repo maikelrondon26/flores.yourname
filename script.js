00692b75817700cf78db2b7fc531ea97b4b930ea
maikelrondon26 <44073025+maikelrondon26@users.noreply.github.com>
2025-10-29 13:48:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  // Eliminar la clase container para iniciar las animaciones
+  const container = document.querySelector('.container');
+  if (container) {
+    container.classList.remove("container");
+  }
+};
